8f0d2733dfda14b932a809d45ab5fe165be31db5
Iseyemi Oluwafemi <iseyemiabrahamf@gmail.com.com>
2023-02-19 05:07:30 -0800

    my commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - A program that print the alphabets in lowercae
+ * Return: 0 (SUCCESS)
+ */
+
+int main(void)
+{
+char current = 'a';
+while (current <= 'z')
+}
+putchar(current);
+current++;
+}
+putchar('\n');
+return (0);
+}
