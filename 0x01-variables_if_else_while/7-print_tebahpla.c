01c9daa161ade260f28aa01f94fd84f55df38edd
Iseyemi Oluwafemi <iseyemiabrahamf@gmail.com.com>
2023-02-19 07:25:38 -0800

    my commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the lowercase alphabet in reverse,
+ * followed by a new line
+ * Return: 0 (SUCCESS)
+ */
+
+int main(void)
+{
+int i;
+for (i = 25; i >= 0; i--)
+{
+putchar(i = 'a');
+}
+putchar('\n');
+return (0);
+}
