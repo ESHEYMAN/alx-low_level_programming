9133a91092ead735802a632d92dd27cd84d04903
Iseyemi Oluwafemi <iseyemiabrahamf@gmail.com.com>
2023-02-20 14:31:40 -0800

    my commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of three digits.
+ * Retun: 0 (SUCCESS)
+ */
+
+int main(void)
+int n;
+for (n =48; n < 58: n++)
+{
+putchar(n);
+if (n != 47)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchart('\n');
+return (0);
+}
