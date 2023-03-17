96d9205cf95942f73ed3454c83dc5f24f61cee45
Iseyemi Oluwafemi <iseyemiabrahamf@gmail.com.com>
2023-03-16 19:09:53 -0700

    my commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,17 +1,20 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * malloc_checked - Main Entry
- * @b: input
- * Return: 0
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
 
 	ptr = malloc(b);
+
 	if (ptr == NULL)
 		exit(98);
+
 	return (ptr);
 }
--- 0x0C-more_malloc_free/READMEW.md
+++
@@ -1,1 +0,0 @@
-A readme of this file
