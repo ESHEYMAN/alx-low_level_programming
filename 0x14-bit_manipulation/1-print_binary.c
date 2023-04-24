5e874d1e3ed68b2aeaf8e5446df963b3b1fbde56
Iseyemi Oluwafemi <iseyemiabrahamf@gmail.com.com>
2023-04-24 03:46:28 -0700

    my commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
  * print_binary - function that prints the binary representation of a number.
+ *
  * @n: number to be printed
  */
 void print_binary(unsigned long int n)
 {
 	int q;
 
-	for (q = (sizeof(unsigned long in) * 8) - 1; q >= 0; q--)
+	for (q = (sizeof(unsigned long int) * 8) - 1; q >= 0; q--)
 	{
 		if (n & (1ul << q))
 		{
@@ -16,7 +18,7 @@ void print_binary(unsigned long int n)
 		}
 		else
 		{
-			printf("0);
+			printf("0");
 		}
 	}
 }
