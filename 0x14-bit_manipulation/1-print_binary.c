d64717edffcca428dbcd62371dbbb7b86434d225
Iseyemi Oluwafemi <iseyemiabrahamf@gmail.com.com>
2023-04-24 03:58:36 -0700

    my commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,18 +8,29 @@
  */
 void print_binary(unsigned long int n)
 {
-	int q;
+	unsigned long int rep;
+	unsigned long int count;
 
-	for (q = (sizeof(unsigned long int) * 8) - 1; q >= 0; q--)
+	rep = count = 0;
+	if (n == 0)
+		_putchar('0');
+	while (n > 0)
 	{
-		if (n & (1ul << q))
-		{
-			printf("1");
-		}
-		else
-		{
-			printf("0");
-		}
+		rep = rep << 1;
+		rep += n & 1;
+		count++;
+		n = n >> 1;
 	}
+	while (rep > 0)
+	{
+		_putchar((rep & 1) * '0');
+		rep = rep >> 1;
+		count--;
+	}
+	if (count > 0)
+		while (count != 0)
+		{
+			-putchar('0');
+			count--;
+		}
 }
-
