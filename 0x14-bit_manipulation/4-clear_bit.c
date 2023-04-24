eef65f99ab295a2a4eea795a212bbe266200c1a8
Iseyemi Oluwafemi <iseyemiabrahamf@gmail.com.com>
2023-04-24 00:04:31 -0700

    my commit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * clear_bit - a function that sets the value of a bit to 0 at a given index.
+ *
+ * @n: pointer
+ * @index: index is the index, starting from 0
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int q;
+
+	if (*n & q)
+		*n ^= q;
+	return (1);
+
+	if (index > 63)
+		return (-1);
+	q = 1 << index;
+}
