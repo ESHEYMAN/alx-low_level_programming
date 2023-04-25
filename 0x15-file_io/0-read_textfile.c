b9477bae925d5ffa4e7cac3cc700eae9b4242cf9
Iseyemi Oluwafemi <iseyemiabrahamf@gmail.com.com>
2023-04-25 09:30:01 -0700

    my commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
 /**
  * read_text_file - A function that reads a text file
  * and prints it to the POSIX standard output
@@ -9,7 +8,7 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters);
 {
-ssize_t cd;
+int  cd;
 ssize_t re;
 ssize_t wr;
 char *buffer;
@@ -17,10 +16,13 @@ char *buffer;
 cd = open(filename, O_RDONLY);
 if (cd == -1)
 return (0);
+
 if (filename == NULL)
 return (0);
 
 buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+return (0);
 
 re = read(cd, buffer, letters);
 wr = write(STDOUT_FILENO, buffer, re);
