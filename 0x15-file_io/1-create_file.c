3b10f39523fd799a443fb29a920d36874776a809
Iseyemi Oluwafemi <iseyemiabrahamf@gmail.com.com>
2023-04-27 02:41:08 -0700

    my commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,28 +1,35 @@
 #include "main.h"
 #include <string.h>
+
 /**
- * create_file - A function that create a file
- * @filename: name of the file
- * @text_content: file to be created
- * Return: 1 on sucess, -1 if error
+ * create_file - creates a file
+ * @filename: name of file to create
+ * @text_content: string to write content
+ * Return: 1 on success || -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fn, w, len = 0;
+	int fd, _write;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	if (text_content != NULL)
+	/* create file*/
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		close(fd);
+		return (1);
 	}
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fn == -1 || -1)
+	_write = write(fd, text_content, strlen(text_content));
+	if (_write == -1 || _write != strlen(text_content))
+	{
+		close(fd);
 		return (-1);
-
-	close(fn);
-
+	}
+	close(fd);
 	return (1);
 }
