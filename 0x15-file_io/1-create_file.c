b0cfa20f2877d182ddf63a3d76c56bbbe0b06764
Iseyemi Oluwafemi <iseyemiabrahamf@gmail.com.com>
2023-04-27 02:26:19 -0700

    create file commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,27 +6,23 @@
  * @text_content: file to be created
  * Return: 1 on sucess, -1 if error
  */
-int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content)
 {
-int fn;
-int _letter;
-int _write;
+	int fn, w, len = 0;
 
-if (filename == NULL)
-	return (-1);
+	if (filename == NULL)
+		return (-1);
 
-fn = open(filename, O_CREAT | O_RDONLY | O_TRUNC, 0600);
-if (fn == -1)
-return (-1);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fn == -1 || -1)
+		return (-1);
 
-if (text_content != NULL)
-{
-for (_letter = 0; text_content[_letter]; _letter++)
+	close(fn);
+
+	return (1);
 }
-
-_write = write(fn, text_content, _letter);
-if (_write == -1)
-return (-1);
-
-close(fn);
-return (1);
