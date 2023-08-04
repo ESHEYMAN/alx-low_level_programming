727e629b171e58cedb625fc2c45a9226c2235532
Iseyemi Oluwafemi <iseyemiabrahamf@gmail.com>
2023-08-03 17:24:36 -0700

    my commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * binary_to_uint - function that converts a binary number to an unsigned int
+ * @b: Apointer to a string
+ *
+ * Return: the converted num, or 0 if error
+ * or there is one more char
+ * b is NULL
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int m = 1;
+	unsigned int i = 0;
+	int q;
+	unsigned int len;
+
+	len = strlen(b);
+
+	for (q = len - 1; q >= 0; q--)
+	{
+		if (b[q] != '0' && b[q] != '1')
+			return (0);
+	if (b[q] == '1')
+	{
+		i += m;
+	}
+	m *= 2;
+	}
+	return (i);
+}
